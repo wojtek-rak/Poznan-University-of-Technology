ec034cac68a22141deec8336881466234a839813
wolix07 <rak.wojtek@wp.pl>
2018-04-23 16:26:01 +0200

    Initial commit

---
+++ Assets/AomController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AomController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () 
+    {
+        float angle = Random.Range(30f, 150f) + Random.Range(0, 2) * 180f;
+        float radAngle = angle * Mathf.Deg2Rad;
+        Vector2 dir = new Vector2(Mathf.Cos(radAngle), Mathf.Sin(radAngle));
+        float xSpin = Random.Range(0, 360);
+        float ySpin = Random.Range(0, 360);
+        Debug.Log(xSpin);
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector2(5, 0);
+        rb.velocity = dir * 4f;
+        //rb.transform.Rotate(new Vector3(0f, 0f, xSpin));
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
---
+++ Assets/AtomSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomSpawner : MonoBehaviour {
+
+    public float atoms;
+    public GameObject atom;
+
+	// Use this for initialization
+	void Start () {
+        float sum_i;
+        float sum_j;
+        for (int j = -4; j <= 4 ; j += 1)
+        { 
+            for (int i = -4; i <= 4; i += 1)
+            {
+                sum_i = j / atoms * 3.5f;
+                sum_j = i / atoms * 3.5f;
+                Instantiate(atom, new Vector3(sum_i, sum_j, 0f), Quaternion.identity);
+            }
+
+        }
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //foreach in atom
+	}
+}
