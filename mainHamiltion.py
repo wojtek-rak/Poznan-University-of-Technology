0e345e50498def76208330cd2adb11cc0592b91a
wolix07 <rak.wojtek@wp.pl>
2018-04-30 16:55:41 +0200

    hamilton_com
    HamiltonCom

---
+++ mainHamiltion.py
@@ -0,0 +1,125 @@
+import time
+import random
+import sys
+sys.setrecursionlimit(1500000000)
+import pprint
+
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+"""
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if not graph.has_key(start):
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths """
+
+kp = 1
+
+def zapis(lists):
+    global  kp
+    print(kp)
+    print(lists)
+    print("done")
+    print()
+    kp += 1
+
+def main():
+    #try:
+    #    plik = open('big.in')
+    #    tekst = plik.read()
+    #    tekst = re.split(r'[ \n]', tekst)
+    #finally:
+    #    plik.close()
+    tab = [100,34,1233,4,39213,1,2334,324,31244,23,423415,0]
+    tab = [5, 20, 20, 24, 63, 51, 6]
+    try:
+        plik = open('data_macierz_hamilton.txt')
+        lst = plik.read()
+        lst = lst.replace("]]", "]")
+        lst = lst.replace("[", "")
+        lst = lst.replace("\n", "")
+        lst = lst.split("next")
+    finally:
+        plik.close()
+
+    wyniki = []
+    mac_list = []
+    temp_list = []
+    for i in lst:
+        temp = i.split("]")
+        for j in temp:
+            temp2 = j.split(" ")
+            temp2 = list(filter(None, temp2))
+            temp2 = list(map(int, temp2))
+            temp_list.append(temp2)
+        temp_list = list(filter(None, temp_list))
+        mac_list.append(temp_list)
+        temp_list = []
+    #print(mac_list)
+    #for i in mac_list:
+    #    for j in i:
+    #        print(j)
+
+    lst_sums = []
+    sum_ham = 0
+    for mac_ham in mac_list:
+        start = time.time()
+        keys = list(range(0, len(mac_ham[0])))
+        #print(keys)
+        mac = dict(zip(keys, zip(*zip(*mac_ham))))
+        #print(mac)
+        for dic in keys:
+            mac[dic] = list(mac[dic])
+        #print(mac)
+        for dic in keys:
+            mac[dic] = [x for x in mac[dic] if x != -1]
+            # try:
+            #    b = mac[dic].index(-1)
+            #    del mac[dic][b]
+            # except:
+            #    pass
+            # print(type(mac[dic]))
+        #print(mac)
+
+        for st in mac[0]:
+            lst = find_all_paths(mac, st, 0)
+            for k in lst:
+                if (len(k) == len(keys)):
+                    sum_ham += 1
+
+
+        #for ke in keys:
+        #    lst = find_all_paths(mac, 0, ke)
+        #    for k in lst:
+        #        if (len(k) == len(keys) and 0 in mac[k[len(keys)-1]]):
+        #            sum_ham += 1
+        lst_sums.append(sum_ham)
+        sum_ham = 0
+        #[0, 4, 16, 8, 20, 482]
+
+        print(time.time() - start)
+        zapis(lst_sums)
+
+
+
+if __name__ == "__main__":
+    main()
--- samamaciezr.py
+++ samamaciezr.py
@@ -10,7 +10,7 @@ def calc(mc, quan):
     sum = 0
     for i in range(quan):
         for j in range(quan):
-            if mc[i][j] != 0:
+            if mc[i][j] != -1:
                 sum += 1
     return sum
 
@@ -19,45 +19,79 @@ def main():
 
     lst = []
 
-    sizeof = 30
+    sizeof = 5
+    numOfMatrix = 15
 
     size = sizeof
     it = 1
-    nas = 0.7
+    nas = 0.5
+    dodaw = False
     break_point = False
-    for kp in range(2):
-        for i in range(6):
-            print(it)
-            x = np.zeros((size, size)).astype(int)
-            while (it < size):
-                for j in range(size - it):
-                    if calc(x, size) / (size * size) > nas:
-                        break_point = True
-                        break
-                    ran = random.randint(1, 1000)
-                    x[j][j + it] = ran
-                    x[j + it][j] = ran
-                if (break_point):
+    for i in range(numOfMatrix):
+        x = np.zeros((size, size)).astype(int)
+        for ke in range(len(x[0])):
+            for re in range(len(x[0])):
+                x[ke][re] = -1
+        while (it < size):
+            for j in range(size - it):
+                if calc(x, size) / (size * size) > nas:
+                    break_point = True
                     break
-                it += 1
+                x[j][j + it] = j + it
+                x[j + it][j] = j
+            if (break_point):
+                break
+            it += 1
+        sum = 0
+        break_sum = False
+        break_if = True
+        while(break_if):
+            for j in range(size):
+                for k in range(size):
+                    if (x[j][k] != -1):
+                        sum += 1
+                print(sum)
+                if (sum % 2 == 1):
+                    for k in range(size):
+                        kap = random.randint(1, size - 1)
+                        if (dodaw):
+                            if (x[j][kap] != -1):
+                                x[j][kap] = -1
+                                x[kap][j] = -1
+                                dodaw = False
+                                break
+                        else:
+                            if (x[j][kap] == -1 and j != kap):
+                                x[j][kap] = kap
+                                x[kap][j] = j
+                                dodaw = True
+                                break
+                sum = 0
+            break_sum = False
+            for j in range(size):
+                for k in range(size):
+                    if (x[j][k] != -1):
+                        sum += 1
+                if ( sum % 2 == 1):
+                    break_sum = True
+                sum = 0
+            if(break_sum == False):
+                break_if = False
 
-            size += sizeof
-            it = 1
-            break_point = False
-            x = np.array2string(x,max_line_width=None)
-            lst.append(x)
-
-        nas = 0.3
-        size = sizeof
+        size += 1
         it = 1
+        break_point = False
+        print (x)
+        x = np.array2string(x, max_line_width=None)
+        lst.append(x)
     print(lst)
     x = np.zeros((3,3)).astype(int)
 
     print(type(x))
     print(x)
 
-    text_file = open("data_macierz.txt", "w")
-    for j in range(12):
+    text_file = open("data_macierz_hamilton.txt", "w")
+    for j in range(numOfMatrix):
         text_file.write("".join(lst[j]))
         text_file.write("\nnext\n")
 
