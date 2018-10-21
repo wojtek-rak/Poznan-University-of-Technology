e50cffc5f3120f0200a25e234e0886fef17807b2
Wolix <Rak.wojtek@wp.pl>
2018-10-21 18:23:57 +0200

    set up

--- PCmax/Program.cs
+++ PCmax/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PCmax
@@ -10,8 +12,34 @@ namespace PCmax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int numberOfThreads;
+            int numberOfTasks;
+            string line;
+            List<int> tasks = new List<int>();
+            List<int> taskMachines = new List<int>();
+            //Initialize data from file
+            using (var reader = new StreamReader("TestInstance.txt"))
+            {
+                numberOfThreads = Int32.Parse(reader.ReadLine());
+                numberOfTasks = Int32.Parse(reader.ReadLine());
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tasks.Add(Int32.Parse(line));
+                }
+            }
+            //Initialize counting for machines
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                taskMachines.Add(0);
+            }
+            Console.WriteLine(@"Number of Tasks: {0}, Number of Threads {1}", numberOfTasks, numberOfThreads);
+            foreach (var task in tasks)
+            {
+                Console.Write(@"{0} ", task);
+            }
+
+
+            Thread.Sleep(8000);
         }
     }
 }
