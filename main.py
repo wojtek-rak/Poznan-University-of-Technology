9f93720a7e1194851e598648b572677ba1612560
Wojciech Rak <rak.wojtek@wp.pl>
2019-12-15 13:54:13 +0100

    Add keypadLib

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+import time
+import RPi.GPIO as GPIO
+from keypad import keypad
 
-
-
-
-x = 1
+GPIO.setwarnings(False)
 
 
 
 def main():
-    print("Hello World!")
-    print (x)
+    # Initialize
+    kp = keypad(columnCount=3)
+
+    # waiting for a keypress
+    digit = None
+    while digit == None:
+        digit = kp.getKey()
+    # Print result
+    print(digit)
+    time.sleep(0.5)
+
+    ###### 4 Digit wait ######
+    seq = []
+    for i in range(4):
+        digit = None
+        while digit == None:
+            digit = kp.getKey()
+        seq.append(digit)
+        time.sleep(0.4)
+
+    # Check digit code
+    print(seq)
+    if seq == [1, 2, 3, '#']:
+        print( "Code accepted")
 
 if __name__== "__main__":
     main()
