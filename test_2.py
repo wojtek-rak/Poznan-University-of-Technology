73ab32a9ef98c077eef54d5d60418d568fb771ed
Wojciech Rak <rak.wojtek@wp.pl>
2019-12-15 16:02:24 +0100

    Add upgrade

--- test_2.py
+++ test_2.py
@@ -14,7 +14,7 @@ lcd_d7 = 18
 lcd_columns = 16
 lcd_rows = 2
 
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows,initial_backlight=0.0)
 lcd.clear()
 time.sleep(5.0)
 lcd.message('Hello\nworld!')
