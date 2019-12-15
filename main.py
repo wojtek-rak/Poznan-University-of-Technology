c220b82d7babc5f0a10f42a2119c82ad974484b1
Wojciech Rak <rak.wojtek@wp.pl>
2019-12-15 14:09:23 +0100

    Working digits

--- main.py
+++ main.py
@@ -4,34 +4,39 @@ from keypad import keypad
 
 GPIO.setwarnings(False)
 
+kp = keypad(columnCount=3)
 
-
-def main():
-    # Initialize
-    kp = keypad(columnCount=3)
-
-    # waiting for a keypress
+def waitForMenu():
     digit = None
     while digit == None:
         digit = kp.getKey()
+        return digit
+
+def mainMenu():
+
+
+    digit = None
+
     # Print result
     print(digit)
     time.sleep(0.5)
 
-    ###### 4 Digit wait ######
-    seq = []
-    for i in range(4):
-        digit = None
-        while digit == None:
-            digit = kp.getKey()
-        seq.append(digit)
-        time.sleep(0.4)
 
-    # Check digit code
-    print(seq)
-    if seq == [1, 2, 3, '#']:
-        print( "Code accepted")
+    #
+    # ###### 4 Digit wait ######
+    # seq = []
+    # for i in range(4):
+    #     digit = None
+    #     while digit == None:
+    #         digit = kp.getKey()
+    #     seq.append(digit)
+    #     time.sleep(0.4)
+    #
+    # # Check digit code
+    # print(seq)
+    # if seq == [1, 2, 3, '#']:
+    #     print( "Code accepted")
 
 if __name__== "__main__":
-    main()
+    mainMenu()
 
