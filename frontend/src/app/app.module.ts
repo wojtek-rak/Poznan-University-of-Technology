d58ec8e192d35f86bde8e16edc80553badcb3a78
Wojciech Rak <rak.wojtek@wp.pl>
2019-12-31 14:04:30 +0100

    Add manager and customer comopnent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ManagerComponent } from './manager/manager.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManagerComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
