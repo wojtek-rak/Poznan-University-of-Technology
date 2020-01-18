092d1a527d0ee96425b1b398afe7f19435f6d16b
Wojciech Rak <rak.wojtek@wp.pl>
2020-01-18 18:56:14 +0100

    add client to dir

---
+++ Client/NetworkCalendar/NetworkCalendar/Models/Day.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetworkCalendar.Models
+{
+    public class Day
+    {
+        public int Number { get; set; }
+        public List<Event> Events { get; set; } = new List<Event>();
+        public bool IsEvent { get; set; } = false;
+    }
+}
---
+++ Client/NetworkCalendar/NetworkCalendar/Models/IpSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetworkCalendar.Models
+{
+    public class IpSettings
+    {
+        public string Ip { get; set; }
+        public int Port { get; set; }
+    }
+}
